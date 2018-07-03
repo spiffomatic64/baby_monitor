e55a745d37b340c61edd49978dd8e369ab5b1698
Spiffomatic64 <root@spiffomatic64.com>
2018-07-03 08:26:33 -0400

    fullscreen mjpg video

---
+++ www/index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<style>
+    body, html {
+        height: 100%;
+        margin: 0;
+    }
+</style>
+
+</head>
+<body bgcolor="#000000">
+<center>
+<img src="http://ipaddressofrpi:8081/" height="100%">
+</center>
+</body>
+</html>
